91efe7e69890f1e3df86dd0897c0458f63e0d8c9
umang-goyal <goel97umang@gmail.com>
2020-01-31 10:35:30 +0530

    implemented code for selection sort, insertion sort, bubble sort algorithms.

--- src/main/scala/edu/knoldus/Sorting.scala
+++ src/main/scala/edu/knoldus/Sorting.scala
@@ -2,19 +2,72 @@ package edu.knoldus
 
 class Sorting {
 
-  def insertionSort(array: Array[Int]): Array[Int] = {
-    //todo: Add Logic
-    array
-  }
+ def selectionSort(array: Array[Int]): Array[Int] = {
+  @scala.annotation.tailrec
+  def sSort(array: Array[Int], intialIndex: Int): Array[Int] = {
+    def findMin(array: Array[Int], startIndex: Int): Int= {
+      for (i <- startIndex until array.length ) {
+        if (array(i) < array(startIndex)) {
+          return findMin(array, i)
+        }
+      }
+      startIndex
+    }
+    def swap(array: Array[Int], i: Int, j: Int) {
+      val tmp = array(i)
+      array(i) = array(j)
+      array(j) = tmp
+    }
 
-  def selectionSort(array: Array[Int]): Array[Int] = {
-    //todo: Add Logic
-    array
+
+    if (intialIndex == array.length){
+      return array
+    }
+    val min = findMin(array, intialIndex)
+    swap(array,intialIndex, min)
+
+    sSort(array, intialIndex+1)
   }
+  sSort(array,0)
+}
 
   def bubbleSort(array: Array[Int]): Array[Int] = {
-    //todo: Add Logic
-    array
+  val n: Int = array.length
+  @scala.annotation.tailrec
+  def bs(array: Array[Int], length: Int): Array[Int]= {
+    def swap(array: Array[Int], index: Int) {
+      val tmp = array(index)
+      array(index) = array(index - 1)
+      array(index - 1) = tmp
+    }
+    for (i <- 1 until length) {
+      if (array(i - 1) >= array(i)) {
+        swap(array,i)
+      }
+    }
+    if (length==1) {
+      array
+    }
+    else {
+      bs(array, length - 1)
+    }
+  }
+  bs(array, n-1)
+  array
+}
+
+  def insertionSort(array: Array[Int]): Array[Int] = {
+  val length: Int = array.length
+  for (i <- 0 until length) {
+    val key = array(i)
+    var j = i
+    while (j > 0 && array(j - 1) > key) {
+      array(j) = array(j - 1)
+      j = j - 1
+    }
+    array(j) = key
   }
 
+  array
+}
 }
